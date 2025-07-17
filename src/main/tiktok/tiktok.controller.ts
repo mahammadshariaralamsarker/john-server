e5afbc89888a3743e0355b30cbc67d6b91e858a3
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-07-18 05:43:00 +0600

    redirect url fix

--- src/main/tiktok/tiktok.controller.ts
+++ src/main/tiktok/tiktok.controller.ts
@@ -41,10 +41,18 @@ export class TikTokController {
         tokenResponse.access_token,
       );
 
-      return res.status(200).json({ user: userInfo });
+      // res.cookie('tiktok_access_token', data.access_token, {
+      //   httpOnly: true,
+      //   secure: true,
+      // });
+
+      // Optionally: redirect to frontend with user info
+      res.redirect(`http://localhost:3001`);
     } catch (err) {
       console.error(err);
       throw new HttpException('Callback failed', HttpStatus.BAD_REQUEST);
     }
   }
 }
+
+// https://www.tiktok.com/v2/auth/authorize?client_key=sbawtbqug63mru0371&response_type=code&scope=user.info.basic,video.upload,video.list&redirect_uri=https%3A%2F%2Fjohn-server.onrender.com%2Fauth%2Ftiktok%2Fcallback&state=3bef09e4-bef5-44af-b4ec-a593d156ef88&code_challenge=qu_JuXSkR3_7fHpKIUbQ85dY7eHW6b5fZkfBKCIbQDk&code_challenge_method=S256&error=invalid_scope&error_type=scope
