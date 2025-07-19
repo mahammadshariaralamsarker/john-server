1eeb36fb470a8bc3cf83fe7f29df984d542b7623
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-07-19 06:34:27 +0600

    successfully added getuser info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,17 +10,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('tiktokp6IiDsjbd96yG7CR65ki9bJ95BdnAthX.txt')
-  serveVerification(@Res() res: Response) {
-    const filePath = join(process.cwd(), 'public', 'tiktok-verification.txt');
-
-    // Set headers to force download
-    res.set({
-      'Content-Type': 'text/plain',
-      'Content-Disposition': 'attachment; filename="tiktok-verification.txt"',
-    });
-
-    return res.sendFile(filePath);
-  }
 }
--- src/main/tiktok/tiktok.service.ts
+++ src/main/tiktok/tiktok.service.ts
@@ -1,14 +1,16 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import axios from 'axios';
 import * as qs from 'querystring';
-
+import * as FormData from 'form-data';
+import { Readable } from 'stream';
 @Injectable()
 export class TiktokService {
   private readonly clientKey = 'sbawtbqug63mru0371';
   private readonly clientSecret = 'hsyZepE0fRpcTD4yG6FV4mi7FfqQGsnk';
   private readonly redirectUri =
     'https://c8d2bd9b655e.ngrok-free.app/auth/tiktok/callback';
-
+  private readonly accessToken =
+    'rft.xLvaKH5Vs9j7mvmhKrFt3mM1gJCsQEG6sO9DBCfKNB2IAYxl7ZR8P3lQ7eg5!4525.va';
   async getAccessToken(code: string) {
     const url = 'https://open.tiktokapis.com/v2/oauth/token/';
     const body = qs.stringify({
@@ -30,93 +32,61 @@ export class TiktokService {
     }
   }
 
-  // Get User Information from tiktok account
-  // async getUserInfo(accessToken: string) {
-  //   try {
-  //     const res = await axios.get('https://open.tiktokapis.com/v2/user/info/', {
-  //       headers: {
-  //         Authorization: `Bearer ${accessToken}`,
-  //       },
-  //     });
-  //     return res.data;
-  //   } catch (err) {
-  //     throw new HttpException(
-  //       err.response?.data || 'Failed to fetch user info',
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-  //   }
-  // }
-
   // post a video on the tiktok
 
-  async publishPost(
-    video: Express.Multer.File,
-    description: string,
-    accessToken: string,
-  ) {
-    console.log({ video, description, accessToken });
+  async getUserInfo(accessToken: string) {
+    const url =
+      'https://open.tiktokapis.com/v2/user/info/?fields=open_id,union_id,avatar_url,display_name';
 
-    // const post_info = {
-    //   title: 'this will be a funny #cat video on your @tiktok #fyp',
-    //   privacy_level: 'MUTUAL_FOLLOW_FRIENDS',
-    //   disable_duet: false,
-    //   disable_comment: true,
-    //   disable_stitch: false,
-    //   video_cover_timestamp_ms: 1000,
-    // };
-    // const source_info = {
-    //   source: 'FILE_UPLOAD',
-    //   video_size: 50000123,
-    //   chunk_size: 10000000,
-    //   total_chunk_count: 5,
-    // };
     try {
-      const initResponse = await axios.post(
-        'https://open.tiktokapis.com/v2/post/publish/video/init/',
-        {
-          post_info: {
-            title: 'this is a #cat video',
-            privacy_level: 'PUBLIC',
-          },
-          source_info: {
-            source: 'FILE_UPLOAD',
-            video_size: 50000123,
-            chunk_size: 10000000,
-            total_chunk_count: 1,
-          },
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
         },
+      });
+
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        error.response?.data || 'Failed to fetch TikTok user info',
+        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async videoInit() {
+    const payload = {
+      post_info: {
+        title: 'This is a test video #tiktok',
+        privacy_level: 'PUBLIC',
+        disable_duet: false,
+        disable_comment: true,
+        disable_stitch: false,
+        video_cover_timestamp_ms: 1000,
+      },
+      source_info: {
+        source: 'FILE_UPLOAD',
+        video_size: 50000123,
+        chunk_size: 10000000,
+        total_chunk_count: 1,
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        'https://open.tiktokapis.com/v2/post/publish/video/init/',
+        payload,
         {
           headers: {
-            Authorization: `Bearer ${'act.IB5KjgoxsyQShqmVmoHHyp0nmgJD9tmYX2UuDLpEqKY8cAP0FAl3KJUJEiHY!4507.va'}`,
+            Authorization: `Bearer ${this.accessToken}`,
             'Content-Type': 'application/json',
           },
         },
       );
-      console.log({ initResponse });
-      // const form = new FormData();
-      // form.append('video', video.buffer, {
-      //   filename: video.originalname,
-      //   contentType: video.mimetype,
-      // });
-      // form.append('description', description);
-      // const response = await axios.post(
-      //   'https://open.tiktokapis.com/v2/post/publish/',
-      //   form,
-      //   {
-      //     headers: {
-      //       ...form.getHeaders(),
-      //       Authorization: `Bearer ${accessToken}`,
-      //     },
-      //     maxContentLength: Infinity,
-      //     maxBodyLength: Infinity,
-      //   },
-      // );
-      // return response.data;
+
+      return response.data;
     } catch (error) {
-      throw new HttpException(
-        error.response?.data || 'TikTok upload failed',
-        error.response?.status || 500,
-      );
+      console.log(error);
     }
   }
 }
